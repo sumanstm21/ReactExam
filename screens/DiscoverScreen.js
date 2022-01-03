6b3d8b28d2ed94051b2e4cf0e1e9962dfafe71c6
Sumanstm21 <sumanstm21@gmail.com>
2022-01-03 14:56:16 +0100

    Basiv navigation

---
+++ screens/DiscoverScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const DiscoverScreen = props => {
+ return (
+    <View>
+        <Text>This is the Discover screen</Text>
+    </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default DiscoverScreen;
